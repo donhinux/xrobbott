ec26e21603b90442924854fe4ffdceb5f3bc74a2
Victor Tozzatto <victor.tozzatto@gmail.com>
2022-06-22 14:02:54 +0100

    testing authentication service token refresh

--- src/services/twitch.auth.service.test.ts
+++ src/services/twitch.auth.service.test.ts
@@ -2,17 +2,20 @@
 *   Should test the totality of the Authentication Service
 */
 import {TwitchAuthService} from "./twitch.auth.service";
+import axios from "axios";
 
-describe('Twitch Authentication Service', ()=>{
+jest.mock('axios');
 
-    it('should verify if there is only a single instance',()=>{
+describe('Twitch Authentication Service', () => {
+
+    it('should verify if there is only a single instance', () => {
         const twitchAuthOriginal = TwitchAuthService.getInstance();
-        twitchAuthOriginal.token ='TEST-TOKEN';
+        twitchAuthOriginal.token = 'TEST-TOKEN';
         const twitchAuthNew = TwitchAuthService.getInstance();
         expect(twitchAuthNew).toBe(twitchAuthOriginal);
     });
 
-    it('should verify if the getter actually returns the current token', ()=>{
+    it('should verify if the getter actually returns the current token', () => {
         const twitchAuthOriginal = TwitchAuthService.getInstance();
         const token = "TEST-TOKEN";
         twitchAuthOriginal.token = token;
@@ -20,7 +23,17 @@ describe('Twitch Authentication Service', ()=>{
         expect(tokenResult).toBe(token);
     });
 
-    it.todo('should verify if the parameters sent to the Twitch API have the right format');
+    it('should verify if the parameters sent to the Twitch API have the right format', async () => {
+        const twitchAuth = TwitchAuthService.getInstance();
+        const params = {
+            "client_id": process.env.TWITCH_CLIENT_ID,
+            "client_secret": process.env.TWITCH_CLIENT_SECRET,
+            "grant_type": TwitchAuthService.GRANT_TYPE,
+        };
+        await twitchAuth.refresh();
+        expect(axios.post).toHaveBeenCalledWith(TwitchAuthService.AUTH_ENDPOINT, null, {params});
+    });
+
     it.todo('should verify if the Twitch API return is valid');
     it.todo('should verify if it includes de possibility of validating the token');
     it.todo('should verify if it includes de possibility of refreshing the token');
--- src/services/twitch.auth.service.ts
+++ src/services/twitch.auth.service.ts
@@ -1,8 +1,12 @@
+import axios from "axios";
+
 export class TwitchAuthService{
 
     private static instance:TwitchAuthService;
-    private accessToken!: string;
+    static readonly AUTH_ENDPOINT: string = "https://id.twitch.tv/oauth2/token";
+    static readonly GRANT_TYPE:string = 'client_credentials';
 
+    private accessToken!: string;
     constructor() { }
 
     static getInstance():TwitchAuthService {
@@ -12,4 +16,14 @@ export class TwitchAuthService{
     }
     get token():string{ return this.accessToken; }
     set token(token){this.accessToken = token;}
+
+    public async refresh(){
+        const params = {
+            "client_id": process.env.TWITCH_CLIENT_ID,
+            "client_secret": process.env.TWITCH_CLIENT_SECRET,
+            "grant_type": TwitchAuthService.GRANT_TYPE,
+        };
+        await axios.post(TwitchAuthService.AUTH_ENDPOINT,null,{params});
+    }
+
 }
