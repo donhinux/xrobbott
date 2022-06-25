db1ab0ef0caa144490433c2575885686c9e0fc54
Victor Tozzatto <victor.tozzatto@gmail.com>
2022-06-25 18:15:31 +0100

    add test and follower method for the user service

--- src/services/twitch.user.service.test.ts
+++ src/services/twitch.user.service.test.ts
@@ -7,7 +7,7 @@ import {TwitchUserService} from "./twitch.user.service";
 jest.mock('axios');
 describe('Twitch User Service', function () {
 
-    const token = 'TEST-TOKEN';
+    const authenticationToken = 'TEST-TOKEN';
     afterEach(() => {
         jest.clearAllMocks();
     });
@@ -21,10 +21,10 @@ describe('Twitch User Service', function () {
             }]
         };
         (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({data: response});
-        const userInfo = await TwitchUserService.getUserInfoByUsername(username, token);
+        const userInfo = await TwitchUserService.getUserInfoByUsername(username, authenticationToken);
         expect(userInfo).toEqual(response.data[0]);
     });
-    it('should touch the Twitch Users API with the right params', async () => {
+    it('should touch the Twitch Users Info endpoint with the right params', async () => {
         const username = "TEST-USER-2";
         const response = {
             data: [{
@@ -33,13 +33,65 @@ describe('Twitch User Service', function () {
             }]
         };
         const headers = {
-            "Authorization": token,
+            "Authorization": authenticationToken,
             "Client-Id": process.env.TWITCH_CLIENT_ID,
         };
         const params = {'login': username};
         (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({data: response});
-        await TwitchUserService.getUserInfoByUsername(username, token);
+        await TwitchUserService.getUserInfoByUsername(username, authenticationToken);
         expect(axios.get).toHaveBeenCalledWith(TwitchUserService.USERS_ENDPOINT, {headers, params});
     });
+
+    it('should fetch a page of followers of a user', async () => {
+        const followers = await TwitchUserService.getUserFollowers('0001', authenticationToken,'PAGINATION-TOKEN');
+        expect(followers.length).toBeLessThanOrEqual(TwitchUserService.FOLLOWS_PAGE_SIZE);
+    });
+    it('should touch the follows endpoint when fetching the followers with expected params', async () => {
+        const userId = "001";
+        const paginationToken = 'PAGINATION-TOKEN';
+        const headers = {
+            "Authorization": authenticationToken,
+            "Client-Id": process.env.TWITCH_CLIENT_ID,
+        };
+        const params = {'to_id': userId, after: paginationToken};
+        await TwitchUserService.getUserFollowers(userId, authenticationToken, paginationToken);
+        expect(axios.get).toHaveBeenCalledWith(TwitchUserService.FOLLOWS_ENDPOINT, {headers, params});
+    });
+    it('should return followers if total count bigger than 0', async ()=>{
+        const userId = "001";
+        const paginationToken = 'PAGINATION-TOKEN';
+        const response = {
+            "total": 2,
+            "data":
+                [
+                    {
+                        "from_id": "171003792",
+                        "from_login": "iiisutha067iii",
+                        "from_name": "IIIsutha067III",
+                        "to_id": "23161357",
+                        "to_name": "LIRIK",
+                        "followed_at": "2017-08-22T22:55:24Z"
+                    },
+                    {
+                        "from_id": "113627897",
+                        "from_login": "birdman616",
+                        "from_name": "Birdman616",
+                        "to_id": "23161357",
+                        "to_name": "LIRIK",
+                        "followed_at": "2017-08-22T22:55:04Z"
+                    },
+                ],
+            "pagination":{
+                "cursor": "eyJiIjpudWxsLCJhIjoiMTUwMzQ0MTc3NjQyNDQyMjAwMCJ9"
+            }
+        };
+        (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({data: response});
+        const followers = await TwitchUserService.getUserFollowers(userId, authenticationToken, paginationToken);
+        expect(followers.length).toEqual(2);
+    });
+
+
+    it.todo('should fetch all the followers of a user');
+    it.todo('should fetch all the follows from a user');
 
 });
--- src/services/twitch.user.service.ts
+++ src/services/twitch.user.service.ts
@@ -2,11 +2,19 @@ import axios from "axios";
 
 export class TwitchUserService {
     static readonly USERS_ENDPOINT: string = "https://api.twitch.tv/helix/users";
+    static readonly FOLLOWS_ENDPOINT: 'https://api.twitch.tv/helix/users/follows';
+    static readonly FOLLOWS_PAGE_SIZE: number = 20;
 
     static async getUserInfoByUsername(username: string, token: string) {
         const headers = {'Authorization': token, 'Client-Id': process.env.TWITCH_CLIENT_ID ?? ''};
         const params = {'login': username};
         const {data} = await axios.get(TwitchUserService.USERS_ENDPOINT, {headers, params});
         return data.data[0];
+    }
+    static async getUserFollowers( userId: string, authenticationToken:string, paginationToken?: string):Promise<any[]> {
+        const headers = {'Authorization': authenticationToken, 'Client-Id': process.env.TWITCH_CLIENT_ID ?? ''};
+        const params = {'to_id': userId, 'after': paginationToken};
+        const {data} = await axios.get(TwitchUserService.FOLLOWS_ENDPOINT, {headers, params});
+        return data.data;
     }
 }
